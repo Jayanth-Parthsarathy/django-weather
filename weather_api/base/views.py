f9cfd17b16b326d8c5ae5836133966e36b30f12b
jayanth-parthsarathy <jparthaa@gmail.com>
2023-02-26 14:15:59 +0530

    Add city model and dummy data

--- weather_api/base/views.py
+++ weather_api/base/views.py
@@ -10,5 +10,11 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
     city = 'Las Vegas'
     city_weather = requests.get(url.format(city, api_key)).json()
-    context = {'weather': city_weather}
+    weather = {
+        'city': city,
+        'temperature': city_weather['main']['temp'],
+        'description': city_weather['weather'][0]['description'],
+        'icon': city_weather['weather'][0]['icon']
+    }
+    context = {'weather': weather}
     return render(request, 'base/index.html', context=context)
